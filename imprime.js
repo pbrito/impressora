32456a3737aba9c9b2e67d085998a1cfbf35b30a
Paulo <pbrito@gmail.com>
2012-10-19 21:30:01 +0100

    stream4

--- imprime.js
+++ imprime.js
@@ -1,6 +1,7 @@
+//  nao sei utilizar
+//-----------------
 var fs = require("fs");
 var txt=["um  ","dois","tres"];
-
 
 
 fs.open("/dev/usblp0", "w+", 0600, function(err, fd) {
@@ -23,29 +24,79 @@ fs.open("/dev/usblp0", "w+", 0600, function(err, fd) {
 
 
 //--------
+
+
 //------------------------
 
-
+var fs = require("fs");
 var stream3 = fs.ReadStream("my_file.txt");
 stream3.setEncoding('ascii');
 stream3.on('data', function(data) {
-console.log(data);
+console.log("on"+data);
     //Do something to the file's data mid stream
 });
 stream3.on('close', function () {
 console.log("fechou")
     //Do something to the file once it's finished reading
-}
-stream.write("My first row\n");
+})
 
-var stream3 = fs.ReadStream("my_file.txt");
-stream3.setEncoding('ascii');
-stream3.on('data', function(data) {
+var fs = require("fs");
+var stream4 = fs.WriteStream("my_file.txt");
+stream4.on('data', function(data) {
 console.log(data);
     //Do something to the file's data mid stream
 });
-stream3.on('close', function () {
+stream4.on('close', function () {
 console.log("fechou")
     //Do something to the file once it's finished reading
+});
+
+stream4.write("My first row\n");
+fs.open("my_file.txt",'ax+' , function(){})
+
+
+// --------------------------
+deferred
+//-------------------------
+
+var jq=require("jquery");
+
+function dump(msg){
+    stream4.write('li  ' + msg );
 }
 
+function doIn(){
+  var defer = jq.Deferred();
+   
+        stream4.write(": Step " + (1) + " finished.\n \n\n");
+        defer.resolve(11);
+     
+    return defer.promise();
+}
+
+function doTask(i,str){
+    var defer = jq.Deferred();
+    setTimeout(function(){
+//        console.log(str+": Step " + (i+1) + " finished.");
+         dump(str+": Step " + (i+1) + " finished.");
+        defer.resolve(i+1);
+    }, Math.random()*1000);
+    return defer.promise();
+}
+
+
+var currentStep = doIn();
+
+var ti=function(str){
+    console.log("prim");
+    for(var i = 1; i < 15; i++){
+        currentStep = currentStep.pipe(function(j){
+           // if(i==0) return doTask(j+1,str);
+            return doTask(j,str);
+        });
+    }
+    jq.when(currentStep).done(function(){
+        console.log("All steps done.");
+         dump("All steps done.");
+    });
+};
